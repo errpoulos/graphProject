b460d1f31392fc6d2d81bd91524d08fa71ad1a19
dsuskauer <dsuskauer3@gmail.com>
2017-12-01 15:09:56 -0500

    Worked on Applicant constructor

--- src/src/Applicant.java
+++ src/src/Applicant.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
+
 public class Applicant extends Vertex {
+    private String name;
+    private int qualificationScore;
+
+    public Applicant(String name, int qualificationScore) {
+        this.name = name;
+        this.qualificationScore = qualificationScore;
+    }
+
 
 }
